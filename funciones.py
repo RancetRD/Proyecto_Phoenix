9b209ad92d05c56133956eed4a1e8132986b0272
Peter <piterrancet11@gmail.com>
2026-03-04 17:14:48 -0400

    Phoenix: Motor de registro 606 y menu funcional

--- funciones.py
+++ funciones.py
@@ -65,8 +65,8 @@ def campo_ncf(mensaje):
       else:
          print("Debe introducir un minimo de 11 caracteres y un limite de 13")
 
-
-def registrar_gasto(facturas):
+#---------------FUNCION BASE DE REGISTRO DE FACTURAS-------------------
+def registrar_gasto(facturas):# ESTA SER LA FUNCION BASE DE LAS FACTURAS
    print("REGISTROS GASTOS 606")
    while True:
         ncf = campo_ncf("NCF-->").upper()
@@ -86,10 +86,7 @@ def registrar_gasto(facturas):
             continue
         break
 
-   isc = campo_float("impuesto selectivo al consumo-->")
-   cdt = campo_float("Contribucion desarrollo Telecom-->")
-   ley_10 = campo_float("Propina Legal 10%:--->")
-   total = monto_neto + itbs + isc + cdt + ley_10
+   total = monto_neto + itbs 
 
    factura = {#AQUI CREAMOS LA LISTA , DE TODO LO QUE TENGA QUE VER CON FACTURA Y DE LA FORMA QUE LA VISUALIZAREMOS
          "proveedor":proveedor,
@@ -98,9 +95,6 @@ def registrar_gasto(facturas):
          "fecha":fecha,
          "monto_neto":monto_neto,
          "itbs":itbs,
-         "isc":isc,
-         "cdt":cdt,
-         "ley_10":ley_10,
          "total":total
            }
    facturas.append(factura)
@@ -110,5 +104,98 @@ def registrar_gasto(facturas):
     
    return facturas
     
+def configurar_empresa():#CON ESTA FUNCION VAMOS A DECIDIR SI EL CLIENTE NECESITA SU DECLARACION FISCA ORDINARIO O RST
+   nombre = campo_texto("Introduca el nombre").strip()
+   rnc = campo_texto("Introduce el RNC")
+   
+   print("Selecciona el regimen fiscal")
+   print("1-RST")
+   print("2-Ordinaria")
+
+   while True:
+    opcion_configuracion = campo_texto("Eliga la opcion 1 o 2-->").strip()
+    if opcion_configuracion not in("1","2"):# NOS ASEGURAMOS DE CUALQUIER ERROR CON ESTA LINEA DE ESPACIO U OPCION DIFERENTE
+      print("Opcion invalida,debe introducir 1 o 2")
+      continue
+   
+    if opcion_configuracion =="1":
+      regimen = "RST"
+    elif opcion_configuracion =="2":
+       regimen = "ordinario"
+    print(f">>Configuracion guardada: {regimen}")
+    break
+   #DEVOLVEMOS LOS DATOS CON RETURN
+   return nombre, rnc, regimen
+#AQUI SE VA ALMACENAR TODOS LOS DATOS QUE TIENE UNA FACTURA
+def guardar_factura_bodega(facturas, ncf, proveedor, rnc, fecha, monto_neto, itbs, isc=0, cdt=0, ley_10=0):
+    total = monto_neto + itbs + isc + cdt + ley_10
+    
+    factura = {
+        "ncf": ncf,           # Empezamos con el NCF como llave principal
+        "proveedor": proveedor,
+        "rnc": rnc,
+        "fecha": fecha,
+        "monto_neto": monto_neto,
+        "itbs": itbs,
+        "isc": isc,
+        "cdt": cdt,
+        "ley_10": ley_10,
+        "total": total     
+    }
+    facturas.append(factura)
+    print(f"\n Registro exitoso. Total: {total}")
+    return facturas
+
+#FUNCION ESPECIALMENTE PARA LOS GASTOS DE RESTAURANTE
+def registrar_telecom(facturas):
+   ncf = campo_ncf("NCF-->").upper()
+   proveedor = campo_texto("Proveedor").strip()
+   rnc = campo_texto("RNC-->").strip()
+   fecha = campo_fecha("Introduzca su fecha, ejm 11/04/2026-->")
+   monto_neto = campo_float("Monto neto-->")
+   itbs = monto_neto * 0.18
+   isc = monto_neto * 0.10
+   cdt = monto_neto * 0.02
+   return guardar_factura_bodega(facturas,ncf,proveedor,rnc,fecha,monto_neto,itbs,isc,cdt)
+
+#FUNCION PARA TIPO GASTO FACTURA RESTAURANTE
+def registrar_restaurante(facturas):
+
+   ncf = campo_ncf("NCF-->").upper()
+   proveedor = campo_texto("Proveedor").strip()
+   rnc = campo_texto("RNC-->").strip()
+   fecha = campo_fecha("Introduzca su fecha, ejm 11/04/2026-->")
+   monto_neto = campo_float("Monto neto-->")
+   itbs = monto_neto * 0.18
+   ley_10 = monto_neto * 0.10
+
+   return guardar_factura_bodega(facturas,ncf,proveedor,rnc,fecha,monto_neto,itbs,0,0,ley_10)
+
+def menu_registro(facturas):#AQUI ES DONDE REGISTRAREMOS EL TIPO DE FACTURA , SEGUN LOS QUE NOS CONVENGAN
+   
+   while True:
+      print("Bienvenido al programa Phoenix")
+      print("Seleccione la opcion del tipo de factura que desea registrar")
+      print("1-Registrar gastos")
+      print("2-Registrar Telecom")
+      print("3-Registrar restaurante")
+      print("4-Salir del menu")
+      opciones = campo_texto("Introduce la opcion que desees registrar").strip()
+      if opciones not in ("1","2","3","4"):
+         print("Opcion invalida,debe digitar un numero")
+         continue
+      if opciones =="1":
+         registrar_gasto(facturas)
+      elif opciones =="2":
+         registrar_telecom(facturas)
+      elif opciones =="3":
+         registrar_restaurante(facturas)
+      elif opciones =="4":
+         print("Saliendo del sistema")
+         break
+      else:
+         print("Opcion no valida")
+      
+
 
     
